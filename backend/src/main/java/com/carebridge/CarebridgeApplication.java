fe15c820f5efb7efe469ccb9d232adf5dfa981e1
ihnaV <ihnz@yahoo.com>
2026-07-13 09:40:33 -0700

    Fix class name typo in CarebridgeApplication

--- backend/src/main/java/com/carebridge/CarebridgeApplication.java
+++ backend/src/main/java/com/carebridge/CarebridgeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CareridgeApplication {
+public class CarebridgeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CareridgeApplication.class, args);
+        SpringApplication.run(CarebridgeApplication.class, args);
     }
 
 }
